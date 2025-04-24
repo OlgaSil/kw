f014ef510dacab2aaaee4e4086511141e389dad3
OlgaSil <101939074+OlgaSil@users.noreply.github.com>
2025-04-24 18:50:41 +0300

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Phones;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
